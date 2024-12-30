2c787d2e3e5a530a336717e82f64a392819f22b2
reokook <amlalhlaly668@gmail.com>
2024-12-30 10:59:47 +0300

    commit

---
+++ one.js
@@ -0,0 +1,48 @@
+let x = 1000;
+let y = 500;
+let result = x / y;
+document.writeln(result);
+////////////////////////
+let z = 1000;
+let a = 500;
+let result2 = z - a;
+document.writeln(result2);
+////////////////////////////
+let b = 1000;
+let c = 500;
+let result3 = b + c;
+document.writeln(result3);
+/////////////////////////////
+let d = 1000;
+let e = 500;
+let result4 = d * e;
+document.writeln(result4);
+/////////////////////////////
+let f = 9;
+let g = 7;
+let t = 2;
+let result5 = (f+g+t) /3;  
+document.writeln(result5);
+/////////////////////////////
+let h = 150;
+let i = 30;
+let first = (150 * 30) / 100;
+let result6 = 150 - first;
+document.writeln(result6);
+/////////////////////////////
+let j = 18;
+let n = 30;
+let m = 20;
+let result7 = j <m< n;
+document.writeln(result7);
+/////////////////////////////
+let num1 = 2;
+let num2 = 3;
+let result8 = (num1**num2);
+document.writeln(result8);
+/////////////////////////////
+let num3 = 4;
+let num4 = 10;
+let result9 = (num4%num3);
+document.writeln(result9);
+/////////////////////////////
